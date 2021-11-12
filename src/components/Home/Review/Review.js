383f2a5f7bab6032f0b9b397fbe785e3e4b4e8d0
OMAR-FARUK-SHAKIL <omarshakil8@gmail.com>
2021-11-13 00:12:42 +0600

    review section updated

--- src/components/Home/Review/Review.js
+++ src/components/Home/Review/Review.js
@@ -1,15 +1,30 @@
 import React from 'react';
+import { Card } from 'react-bootstrap';
 
 const Review = (props) => {
-    const {name,title,location,description} = props.review;
+    const { name, title, location, description } = props.review;
     return (
-        <div className="card col-md-3 col-sm-6 col-xs-12 text-center border border-info ms-5 p-2">
-            <h4>{name}</h4>
-            <h5 className="mt-3 mb-3">From-{location}</h5>
-            <br/><br/>
-            <h3>{title}</h3>
-            <p className="text-secondary"> {description}</p>
-        </div>
+        // <div className="card col-md-3 col-sm-6 col-xs-12 text-center border border-info ms-5 p-2">
+        //     <h4>{name}</h4>
+        //     <h5 className="mt-3 mb-3">From-{location}</h5>
+        //     <br/><br/>
+        //     <h3>{title}</h3>
+        //     <p className="text-secondary"> {description}</p>
+        // </div>
+        <Card style={{ width: '18rem' }} border="info">
+        {/* <Card className="card col-md-3 col-sm-6 col-xs-12 text-center border border-info ms-5 p-2"> */}
+            <Card.Body>
+                <Card.Title className="text-brand bold fw-bold text-info mb-4">{title}</Card.Title>
+                {/* <Card.subtitle>{name}</Card.subtitle> */}
+                <Card.Subtitle className="mb-2 text-muted">{name}</Card.Subtitle>
+                <Card.Subtitle className="mb-2 text-muted">From-{location}</Card.Subtitle>
+                <Card.Text className="mt-3 mb-3">
+                {description}
+                </Card.Text>
+                <Card.Link href="#">Details</Card.Link>
+                <Card.Link href="#">Another Link</Card.Link>
+            </Card.Body>
+        </Card>
     );
 };
 
--- src/components/Home/Reviews/Reviews.js
+++ src/components/Home/Reviews/Reviews.js
@@ -4,6 +4,11 @@ import wilson from '../../../images/wilson.png';
 import ema from '../../../images/ema.png';
 import aliza from '../../../images/aliza.png';
 import Review from '../Review/Review';
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper.min.css";
+import "swiper/components/pagination/pagination.min.css";
+import SwiperCore, { Autoplay, Pagination } from 'swiper/core';
+import { Col, Spinner } from 'react-bootstrap';
 
 const testimonialData = [
     {
@@ -24,11 +29,27 @@ const testimonialData = [
         from : 'California',
         img : aliza
     }
+    ,
+    {
+        quote : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic non architecto nobis, adipisci recusandae repellat accusantium consequuntur, qui nisi deserunt blanditiis mollitia, illo! ',
+        name : 'Ema Gomez',
+        from : 'California',
+        img : ema
+    },
+    {
+        quote : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic non architecto nobis, adipisci recusandae repellat accusantium consequuntur, qui nisi deserunt blanditiis mollitia, illo! ',
+        name : 'Ema Gomez',
+        from : 'California',
+        img : ema
+    }
 ]
 
 
 const Reviews = () => {
     const [reviews,setReviews] = useState([]);
+    SwiperCore.use([Pagination, Autoplay]);
+    // const [reviews, setReviews] = useState([])
+
 
     useEffect(() => {
         fetch("https://shrouded-thicket-71402.herokuapp.com/reviews")
@@ -43,7 +64,7 @@ const Reviews = () => {
                    <h5 className="text-primary text-uppercase">User Reviews</h5>
                    <h1>What Our Patients <br/> Says </h1><hr/>
                </div>
-               <div className="w-80 row mt-3 pt-5 text-center d-flex justify-content-center">
+               {/* <div className="w-80 row mt-3 pt-5 text-center d-flex justify-content-center">
                     {reviews.length === 0 && (     
                         <div style={{textAlign: 'center'}} class="spinner-border text-info" role="status">
                             <span class="visually-hidden">Loading...</span>
@@ -53,7 +74,56 @@ const Reviews = () => {
                     {
                         reviews.map(review => <Review review={review} key={review.name}/>)
                     }
-                </div>
+                </div> */}
+                <Col md={11} className="mx-auto">
+                <Swiper 
+                    pagination={{ clickable: true }}
+                    slidesPerView={3}
+                    breakpoints={{
+                        320: {
+                            slidesPerView: 1,
+                            spaceBetween: 3,
+                        },
+                        768: {
+                            slidesPerView: 2,
+                            spaceBetween: 10,
+                        },
+                        1024: {
+                            slidesPerView: 3,
+                            spaceBetween: 20,
+                        },
+                    }}
+                    autoplay={{
+                        delay: 2500,
+                        disableOnInteraction: false,
+                    }}
+                    spaceBetween={10}
+                    >
+                    
+                    {
+                        reviews.length === 0 ? 
+                            <div className="text-center">
+                                <Spinner animation="border" variant="success" />
+                            </div>: 
+                            reviews.map(review => {
+                                return(
+                                    <SwiperSlide>
+                                           {/* reviews.map(review => <Review review={review} key={review.name}/>) */}
+                                        <Review review={review}  key={review.name}/>
+                                    </SwiperSlide>
+                                )
+                        })
+                    //     testimonialData.map(review => {
+                    //         return(
+                    //             <SwiperSlide>
+                    //                    {/* reviews.map(review => <Review review={review} key={review.name}/>) */}
+                    //                 <Review review={review}  key={review.name}/>
+                    //             </SwiperSlide>
+                    //         )
+                    // })
+                    }
+                </Swiper>
+                </Col>
            </div>
        </section>
     );
